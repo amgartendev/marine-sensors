02b2aa0036a8fe2a985c9a8974979476bbf46e3d
João Amgarten <joaoamgarten0@gmail.com>
2022-08-03 20:01:46 +0100

    Login Page Style

--- login.php
+++ login.php
@@ -5,17 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="_cdn/login_style.css">
     <title>Login</title>
 </head>
 
 <body>
-    <form method="POST" enctype="multipart/form-data">
-        <input name="email" type="email" placeholder="E-mail" required />
-        <input name="password" type="password" placeholder="Senha" required />
 
-        <br><br>
-        <input name="button" type="submit" value="Login" />
-    </form>
+    <img src="images/background_lisbon.png" alt="background" width="100%" />
+
+    <div class="tela-login">
+        <form method="POST" enctype="multipart/form-data">
+            <h1>LOGIN</h1>
+            <input name="email" type="email" placeholder="E-mail" required />
+            <br><br>
+            <input name="password" type="password" placeholder="Senha" required />
+            <br><br>
+            <input name="button" id="button" type="submit" value="Login" />
+        </form>
+    </div>
 </body>
 </html>
 
--- script.js
+++ script.js
@@ -5,17 +5,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([39.3760698, -9.3790874, 21]).addTo(map)
-    .bindPopup('<table><tr><td>TESTE1</td></tr><tr><td>TESTE2</td></tr><tr><td>TESTE3</td></tr></table>')
+    .bindPopup('<table><tr><td>SENSOR 1</td></tr><tr><td>Temperatura: 17.3º C</td></tr><tr><td>Vento: 11 NÓS</td></tr></table>')
     .openPopup();
 
 L.marker([38.6905353, -9.1722086, 19.25]).addTo(map)
-    .bindPopup('<table><tr><td>TESTE1</td></tr><tr><td>TESTE2</td></tr><tr><td>TESTE3</td></tr></table>')
+    .bindPopup('<table><tr><td>SENSOR 2</td></tr><tr><td>Temperatura: 19.0º C</td></tr><tr><td>Vento: 11.5 NÓS</td></tr></table>')
     .openPopup();
 
 L.marker([41.6935403, -8.8093018, 18]).addTo(map)
-    .bindPopup('<table><tr><td>TESTE1</td></tr><tr><td>TESTE2</td></tr><tr><td>TESTE3</td></tr></table>')
+    .bindPopup('<table><tr><td>SENSOR 3</td></tr><tr><td>Temperatura: 15.7º C</td></tr><tr><td>Vento: 13 NÓS</td></tr></table>')
     .openPopup();
 
 L.marker([39.6042563, -9.0857277, 21]).addTo(map)
-    .bindPopup('<table><tr><td>TESTE1</td></tr><tr><td>TESTE2</td></tr><tr><td>TESTE3</td></tr></table>')
+    .bindPopup('<table><tr><td>SENSOR 4</td></tr><tr><td>Temperatura: 9.5º C</td></tr><tr><td>Vento: 13.3 NÓS</td></tr></table>')
+    .openPopup();
+
+L.marker([36.9600594, -7.8869711, 21]).addTo(map)
+    .bindPopup('<table><tr><td>SENSOR 5</td></tr><tr><td>Temperatura: 6.0º C</td></tr><tr><td>Vento: 12 NÓS</td></tr></table>')
     .openPopup();
